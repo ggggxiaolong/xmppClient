ba633117e04a8bdf4c66ec796249918620246b26
mrtan <1120610800@qq.com>
2016-10-05 22:44:33 +0800

    修改访问权限

--- app/src/main/java/com/github/ggggxiaolong/xmpp/datasource/model/Chat.java
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/datasource/model/Chat.java
@@ -1,9 +1,7 @@
 package com.github.ggggxiaolong.xmpp.datasource.model;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 
 import com.github.ggggxiaolong.xmpp.datasource.local.DataBase;
 import com.squareup.sqldelight.RowMapper;
@@ -11,33 +9,19 @@ import com.squareup.sqldelight.RowMapper;
 import org.immutables.value.Value;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-/**
- * Created by mrtan on 10/3/16.
- */
+
 @Value.Immutable
 @Value.Style(allParameters = true)
 public abstract class Chat implements ChatModel {
 
-    public static final ChatModel.Factory<Chat> FACTORY = new Factory<>(ImmutableChat::of);
-    public static final RowMapper<Session> FOR_CHAT_MAPPER = FACTORY.for_chatMapper(new For_chatCreator<Chat, Contact, Session>() {
-        @Override
-        public Session create(@NonNull Chat chat, @NonNull Contact contact, @Nullable Long max_time) {
-            return ImmutableSession.of(chat, contact, max_time);
-        }
-    }, Contact.FACTORY);
+    private static final ChatModel.Factory<Chat> FACTORY = new Factory<>(ImmutableChat::of);
 
+    private static final RowMapper<Session> FOR_CHAT_MAPPER = FACTORY.for_chatMapper(ImmutableSession::of, Contact.FACTORY);
 
     public static long insert(ImmutableChat chat) {
         return DataBase.getWritableDB().insert(TABLE_NAME, null, FACTORY.marshal(chat).asContentValues());
-    }
-
-    public static long insert(String from, String type) {
-        long time = new Date().getTime();
-        ContentValues values = FACTORY.marshal().from_id(from).type(type).time(time).asContentValues();
-        return DataBase.getWritableDB().insert(TABLE_NAME, null, values);
     }
 
     //根据用户查询，与该用户的所有对话
@@ -64,5 +48,6 @@ public abstract class Chat implements ChatModel {
 
     @Value.Immutable
     @Value.Style(allParameters = true)
-    public interface Session extends For_chatModel<Chat, Contact> {}
+    public interface Session extends For_chatModel<Chat, Contact> {
+    }
 }
