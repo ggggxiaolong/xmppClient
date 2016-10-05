db837f4b9e70a1001d7c7de4e9f4b689cc1873cc
mrtan <1120610800@qq.com>
2016-10-05 23:08:35 +0800

    修改了很多

--- app/src/main/java/com/github/ggggxiaolong/xmpp/utils/XMPPUtil.java
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/utils/XMPPUtil.java
@@ -1,16 +1,24 @@
 package com.github.ggggxiaolong.xmpp.utils;
 
-import android.content.Intent;
-
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.chat.Chat;
+import org.jivesoftware.smack.chat.ChatManager;
+import org.jivesoftware.smack.chat.ChatManagerListener;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.roster.Roster;
+import org.jivesoftware.smack.roster.RosterEntry;
+import org.jivesoftware.smack.roster.RosterListener;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
 import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Set;
 
 import timber.log.Timber;
 
@@ -22,26 +30,22 @@ import static android.text.TextUtils.isEmpty;
 
 public final class XMPPUtil {
 
-    public static XMPPTCPConnection connection;
-    
-    /*public static void connect(String name, String address, int port) throws IOException, XMPPException, SmackException {
-        Timber.i("connect to service");
-        if (!isConnected()) {
-            Timber.i("connect not null");
-            closeConnected();
-        }
-        XMPPTCPConnectionConfiguration.Builder builder = XMPPTCPConnectionConfiguration.builder();
-        builder.setServiceName(name);
-        builder.setHost(address);
-        builder.setPort(port);
-        builder.setConnectTimeout(2000);
-        //todo 添加证书
-        builder.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
-        final XMPPTCPConnection connection = new XMPPTCPConnection(builder.build());
-        connection.connect();
-        connection = connection;
-    }*/
+    /**
+     * 服务器连接实体
+     */
+    private static XMPPTCPConnection connection;
+    private static Roster mRoster;
+    private static ChatManager mChatManager;
+    private static final HashMap<String, String> CHAT_MAP = new HashMap<>();
+    private static final MChatManagerListener mChatManagerListener = new MChatManagerListener();
 
+    /**
+     * 根据服务器名，地址，端口登陆
+     *
+     * @throws IOException
+     * @throws XMPPException
+     * @throws SmackException
+     */
     public static void connect(String name, String address, int port, ConnectionListener listener) throws IOException, XMPPException, SmackException {
         Timber.i("connect to service");
         if (isConnected()) {
@@ -53,7 +57,7 @@ public final class XMPPUtil {
         builder.setServiceName(name);
         builder.setHost(address);
         builder.setPort(port);
-        builder.setConnectTimeout(2000);
+        builder.setConnectTimeout(5000);
         //todo 添加证书
         builder.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
         final XMPPTCPConnection conn = new XMPPTCPConnection(builder.build());
@@ -64,21 +68,159 @@ public final class XMPPUtil {
         connection = conn;
     }
 
-    public static void login( String userName, String password) throws IOException, XMPPException, SmackException {
-        if (!isConnected()){
+    /**
+     * 初始化花名册，会话管理器实体
+     */
+    private static void init() {
+        mRoster = Roster.getInstanceFor(XMPPUtil.connection);
+        mChatManager = ChatManager.getInstanceFor(XMPPUtil.connection);
+    }
+
+    /**
+     * 添加花名册监听
+     *
+     * @param listener 监听
+     * @return 结果
+     */
+    public static boolean addRosterListener(RosterListener listener) {
+        if (mRoster != null) {
+            mRoster.addRosterListener(listener);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 取消花名册监听
+     *
+     * @param listener 监听
+     * @return 结果
+     */
+    public static boolean removeRosterListener(RosterListener listener) {
+        if (mRoster != null) {
+            mRoster.removeRosterListener(listener);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 添加会话监听
+     *
+     * @param listener 监听
+     * @return 结果
+     */
+    public static boolean addChatManagerListener(ChatManagerListener listener) {
+        if (mChatManager != null) {
+            mChatManager.addChatListener(listener);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 取消会话监听
+     *
+     * @param listener 监听
+     * @return 结果
+     */
+    public static boolean removeChatManagerListener(ChatManagerListener listener) {
+        if (mChatManager != null) {
+            mChatManager.removeChatListener(listener);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 通过地址查询entry
+     *
+     * @param address 地址
+     * @return entry
+     */
+    public static RosterEntry getEntry(String address) {
+        if (mRoster == null) {
+            return null;
+        }
+        return mRoster.getEntry(address);
+    }
+
+    /**
+     * 获取所有的entry
+     *
+     * @return 列表
+     */
+    public static Set<RosterEntry> getEntries() {
+        if (mRoster == null) {
+            return null;
+        }
+        return mRoster.getEntries();
+    }
+
+    /**
+     * 获取entry 的 状态
+     *
+     * @param userId id
+     * @return 状态
+     */
+    public static Presence getPresence(String userId) {
+        if (mRoster == null) {
+            return null;
+        }
+        return mRoster.getPresence(userId);
+    }
+
+    /**
+     * 获取会话
+     *
+     * @param userId toId
+     * @return chat
+     */
+    public static Chat getChat(String userId) {
+        Chat chat = null;
+        String threadId = CHAT_MAP.get(userId);
+        if (threadId != null) {
+            chat = mChatManager.getThreadChat(threadId);
+        }
+        if (chat == null) {
+            chat = mChatManager.createChat(userId);
+        }
+        CHAT_MAP.put(userId, chat.getThreadID());
+        return chat;
+    }
+
+    /**
+     * 登陆
+     *
+     * @param userName 用户名
+     * @param password 密码
+     * @throws IOException
+     * @throws XMPPException
+     * @throws SmackException
+     */
+    public static void login(String userName, String password) throws IOException, XMPPException, SmackException {
+        if (!isConnected()) {
             throw new RuntimeException("call the connect first!");
         }
         connection.login(userName, password, "android");
+        init();
+        Timber.i("login success");
     }
 
-    public static boolean login(){
+    /**
+     * 根据缓存的账号密码登陆
+     *
+     * @return 结果
+     */
+    public static boolean login() {
         String username = PreferencesUtils.getString(CommonField.USER_NAME);
         String password = PreferencesUtils.getString(CommonField.USER_PASSWORD);
-        if (!isEmpty(username) && isEmpty(password)){
+        if (!isEmpty(username) && !isEmpty(password)) {
             try {
-                connection.login(username, password, "android");
+                login(username, password);
                 return true;
             } catch (Exception e) {
+                Timber.e("login fail");
                 Timber.e(e);
                 return false;
             }
@@ -86,6 +228,15 @@ public final class XMPPUtil {
         return false;
     }
 
+    /**
+     * 根据保存的服务器信息登陆
+     *
+     * @param listener 监听
+     * @return 结果
+     * @throws IOException
+     * @throws XMPPException
+     * @throws SmackException
+     */
     public static boolean connect(ConnectionListener listener) throws IOException, XMPPException, SmackException {
         String name = PreferencesUtils.getString(CommonField.SERVER_NAME);
         String address = PreferencesUtils.getString(CommonField.SERVER_ADDRESS);
@@ -98,33 +249,62 @@ public final class XMPPUtil {
         return true;
     }
 
+    /**
+     * 添加连接监听
+     *
+     * @param listener 监听
+     */
     public static void addConnectionListener(ConnectionListener listener) {
         if (connection != null) {
             connection.addConnectionListener(listener);
         }
     }
 
+    /**
+     * 取消连接监听
+     *
+     * @param listener 监听
+     */
     public static void removeConnectionListener(ConnectionListener listener) {
         if (connection != null) {
             connection.removeConnectionListener(listener);
         }
     }
 
+    /**
+     * 是否与服务器建立连接
+     *
+     * @return 结果
+     */
     public static boolean isConnected() {
         return connection != null && connection.isConnected();
     }
 
+    /**
+     * 是否登陆
+     *
+     * @return 结果
+     */
     public static boolean isLogin() {
         return connection != null && connection.isConnected() && connection.isAuthenticated();
     }
 
+    /**
+     * 主动关闭连接
+     */
     public static void closeConnected() {
         if (isConnected()) {
+            mRoster = null;
+            mChatManager = null;
+            CHAT_MAP.clear();
             connection.disconnect();
             connection = null;
         }
     }
 
+    /**
+     * 包装类
+     */
     public static class ConnectionListenerWrapper implements ConnectionListener {
         //连接到服务器成功
         @Override
@@ -161,10 +341,23 @@ public final class XMPPUtil {
             Timber.i("reconnectingIn");
 
         }
+
         //重连失败
         @Override
         public void reconnectionFailed(Exception e) {
             Timber.i("reconnectionFailed");
+        }
+    }
+
+    public static class MChatManagerListener implements ChatManagerListener {
+
+        @Override
+        public void chatCreated(Chat chat, boolean createdLocally) {
+            if (createdLocally) {
+                Timber.i("主动创建了一个会话, threadId = %s", chat.getThreadID());
+            } else {
+                Timber.i("被动创建了一个会话, threadId = %s", chat.getThreadID());
+            }
         }
     }
 }
