0e86fb0a425a0feef853c4c0e832c07cbd3ee0a9
mrtan <1120610800@qq.com>
2016-10-05 23:01:49 +0800

    修改了它的位置

---
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/chat/MainActivity.java
@@ -0,0 +1,53 @@
+package com.github.ggggxiaolong.xmpp.chat;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.ggggxiaolong.xmpp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.app_bar)
+    AppBarLayout mAppBar;
+    @BindView(R.id.pager)
+    ViewPager mPager;
+    @BindView(R.id.tab)
+    TabLayout mTab;
+    @BindView(R.id.root)
+    CoordinatorLayout mRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        MainPagerAdapter pagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+        mTab.setupWithViewPager(mPager);
+        MainPagerAdapter.setTabIcon(mTab);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
