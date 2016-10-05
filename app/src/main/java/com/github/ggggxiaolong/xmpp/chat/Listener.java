e83d7b81bf4e8ad0143c2a7a58c631a34180893c
mrtan <1120610800@qq.com>
2016-10-05 23:00:35 +0800

    抽出通用的代码

---
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/chat/Listener.java
@@ -0,0 +1,10 @@
+package com.github.ggggxiaolong.xmpp.chat;
+
+import android.view.View;
+
+public interface Listener {
+    
+    interface OnItemClickListener {
+        void onClick(View view, int position);
+    }
+}
