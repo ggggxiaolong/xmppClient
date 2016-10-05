706cb853c2a1f66fc3f21ea1a277fd33f26cdb3b
mrtan <1120610800@qq.com>
2016-10-05 23:08:21 +0800

    修改了很多

--- app/src/main/java/com/github/ggggxiaolong/xmpp/service/XMPPService.java
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/service/XMPPService.java
@@ -7,6 +7,10 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 
+import com.github.ggggxiaolong.xmpp.datasource.model.Contact;
+import com.github.ggggxiaolong.xmpp.datasource.model.ImmutableChat;
+import com.github.ggggxiaolong.xmpp.datasource.model.ImmutableContact;
+import com.github.ggggxiaolong.xmpp.utils.Common;
 import com.github.ggggxiaolong.xmpp.utils.CommonField;
 import com.github.ggggxiaolong.xmpp.utils.JobUtil;
 import com.github.ggggxiaolong.xmpp.utils.ObjectHolder;
@@ -14,72 +18,43 @@ import com.github.ggggxiaolong.xmpp.utils.ThreadUtil;
 import com.github.ggggxiaolong.xmpp.utils.XMPPUtil;
 
 import org.jivesoftware.smack.ConnectionListener;
-import org.jivesoftware.smack.SmackException;
-import org.jivesoftware.smack.StanzaListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.chat.Chat;
-import org.jivesoftware.smack.chat.ChatManager;
 import org.jivesoftware.smack.chat.ChatManagerListener;
 import org.jivesoftware.smack.chat.ChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.packet.Stanza;
-import org.jivesoftware.smack.roster.Roster;
 import org.jivesoftware.smack.roster.RosterEntry;
 import org.jivesoftware.smack.roster.RosterListener;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Set;
 
 import timber.log.Timber;
 
+import static android.text.TextUtils.isEmpty;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.HOLDER_OTHER;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.INTENT_CONTENT;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.INTENT_FORM;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.INTENT_HOLDER;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.INTENT_TIME;
+import static com.github.ggggxiaolong.xmpp.utils.CommonField.RECEIVER_CONTACT;
+
 public class XMPPService extends Service {
 
-    private ChatManager mChatManager;
-    private Roster mRoster;
     private LocalBroadcastManager mBroadcastManager;
+    private Intent mIntent;
 
     @Override
     public void onCreate() {
         mBroadcastManager = ObjectHolder.getBroadcastManager();
+        mIntent = new Intent(CommonField.RECEIVER_CHAT);
         Timber.i("service on create");
         if (!XMPPUtil.isConnected()) {
             Timber.e("XMPP connection was not connected!");
-            return;
         }
-
-        ThreadUtil.runONWorkThread(new Runnable() {
-            @Override
-            public void run() {
-                Timber.i("同步花名册");
-                mRoster = Roster.getInstanceFor(XMPPUtil.connection);
-                Set<RosterEntry> entries = mRoster.getEntries();
-                for (RosterEntry entry : entries) {
-                    Timber.i("groups : %s ;", entry.getGroups().get(0).getName());
-                    Timber.i("name : %s ;", entry.getName());
-                    Timber.i("status : %s ;", entry.getStatus());
-                    Timber.i("type : %s ;", entry.getType());
-                    Timber.i("user : %s \n", entry.getUser());
-                }
-                //监听花名册的变更
-
-                mRoster.addRosterListener(mRosterListener);
-                //监听消息的改变
-                mChatManager = ChatManager.getInstanceFor(XMPPUtil.connection);
-
-                mChatManager.addChatListener(mChatManagerListener);
-
-                XMPPUtil.connection.addAsyncStanzaListener(new StanzaListener() {
-                    @Override
-                    public void processPacket(Stanza packet) throws SmackException.NotConnectedException {
-                        Timber.i(packet.toString());
-                    }
-                }, null);
-
-                XMPPUtil.connection.addConnectionListener(mConnectionListener);
-            }
-        });
-        super.onCreate();
     }
 
     @Nullable
@@ -91,13 +66,62 @@ public class XMPPService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Timber.i("onStartCommand");
+        Timber.i("connect is null: %s", !XMPPUtil.isConnected());
+        if (XMPPUtil.isConnected()) {
+            Timber.i("add listener");
+            XMPPUtil.addConnectionListener(mConnectionListener);
+            if (XMPPUtil.isLogin()) {
+                //监听花名册的变更
+                XMPPUtil.addRosterListener(mRosterListener);
+                //监听消息的改变
+                XMPPUtil.addChatManagerListener(mChatManagerListener);
+            }
+            final Set<RosterEntry> entries = XMPPUtil.getEntries();
+            if (entries == null || entries.isEmpty()) {
+                return START_STICKY;
+            }
+            ThreadUtil.runONWorkThread(new Runnable() {
+                @Override
+                public void run() {
+                    Timber.i("同步花名册: %s", entries.size());
+                    ArrayList<Contact> contacts = new ArrayList<>();
+                    for (RosterEntry entry : entries) {
+                        contacts.add(getContact(entry));
+                    }
+                    Contact.insertAll(contacts);
+                }
+            });
+        }
         return START_STICKY;
+    }
+
+    private ImmutableContact getContact(RosterEntry entry) {
+        String user = entry.getUser();
+        ImmutableContact.Builder builder = ImmutableContact.builder()
+                .user_name(entry.getName())
+                .pinyin(Common.toPinYin(user))
+                .avatar(null)
+                .user_id(user);
+        Presence presence = XMPPUtil.getPresence(user);
+        if (presence != null) {
+            builder.p_mode(presence.getMode())
+                    .p_status(presence.getStatus())
+                    .p_type(presence.getType());
+        }
+        return builder.build();
     }
 
     @Override
     public void onDestroy() {
         Timber.i("onDestroy");
-        super.onDestroy();
+        //监听花名册的变更
+        XMPPUtil.removeRosterListener(mRosterListener);
+        //监听消息的改变
+        XMPPUtil.removeChatManagerListener(mChatManagerListener);
+        if (XMPPUtil.isConnected()) {
+            XMPPUtil.removeConnectionListener(mConnectionListener);
+        }
+        XMPPUtil.closeConnected();
     }
 
     public class Inner extends Binder {
@@ -110,32 +134,37 @@ public class XMPPService extends Service {
             Timber.i("花名册增加了");
             for (String address : addresses) {
                 Timber.i("address : %s", address);
+                RosterEntry entry = XMPPUtil.getEntry(address);
+                ImmutableContact contact = getContact(entry);
+                Contact.insertOrUpdate(contact);
             }
+            mBroadcastManager.sendBroadcast(new Intent(RECEIVER_CONTACT));
         }
 
         @Override
         public void entriesUpdated(Collection<String> addresses) {
             Timber.i("花名册更新了");
             for (String address : addresses) {
-                Timber.i("address : %s", address);
+                RosterEntry entry = XMPPUtil.getEntry(address);
+                ImmutableContact contact = getContact(entry);
+                Contact.update(contact);
             }
+            mBroadcastManager.sendBroadcast(new Intent(RECEIVER_CONTACT));
         }
 
         @Override
         public void entriesDeleted(Collection<String> addresses) {
             Timber.i("花名册减少了");
             for (String address : addresses) {
-                Timber.i("address : %s", address);
+                Contact.delete(address);
             }
+            mBroadcastManager.sendBroadcast(new Intent(RECEIVER_CONTACT));
         }
 
         @Override
         public void presenceChanged(Presence presence) {
             Timber.i("状态改变了");
-            Timber.i("目的地 : %s", presence.getTo());
-            Timber.i("发起者 : %s", presence.getFrom());
-            Timber.i("status : %s", presence.getStatus());
-            Timber.i("mode : %s", presence.getMode());
+            Contact.update(presence.getFrom(), presence.getStatus(), presence.getMode(), presence.getType());
         }
     };
 
@@ -143,24 +172,41 @@ public class XMPPService extends Service {
         @Override
         public void chatCreated(Chat chat, boolean createdLocally) {
             if (createdLocally) {
-                Timber.i("主动创建了一个会话");
+                Timber.i("主动创建了一个会话, threadId = %s", chat.getThreadID());
             } else {
-                Timber.i("被动创建了一个会话");
+                Timber.i("被动创建了一个会话, threadId = %s", chat.getThreadID());
             }
-            Timber.i("会话ID : %s", chat.getThreadID());
-
             chat.addMessageListener(mChatMessageListener);
+            Timber.i("会话ID : %s", chat.getThreadID());
         }
     };
     private ChatMessageListener mChatMessageListener = new ChatMessageListener() {
+
         @Override
         public void processMessage(Chat chat, Message message) {
+            String body = message.getBody();
+            if (isEmpty(body)) {
+                return;
+            }
+            final String from = message.getFrom();
+            String fromId = from.substring(0, from.indexOf('/'));
+
+            long time = new Date().getTime();
             Timber.i("会话ID：%s", chat.getThreadID());
-            Timber.i("会话发起者：%s", message.getFrom());
+            Timber.i("会话发起者：%s", from);
             Timber.i("会话目标：%s", message.getTo());
             Timber.i("会话主题：%s", message.getSubject());
             Timber.i("会话类型：%s", message.getType());
-            Timber.i("会话内容：%s", message.getBody());
+            Timber.i("会话内容：%s", body);
+            mIntent.putExtra(INTENT_FORM, fromId);
+            mIntent.putExtra(INTENT_CONTENT, body);
+            mIntent.putExtra(INTENT_TIME, time);
+            mIntent.putExtra(INTENT_HOLDER, HOLDER_OTHER);
+            mBroadcastManager.sendBroadcast(mIntent);
+            ThreadUtil.runONWorkThread(() -> {
+                ImmutableChat chat1 = ImmutableChat.builder().from_id(fromId).content(body).holder(false).time(time).build();
+                com.github.ggggxiaolong.xmpp.datasource.model.Chat.insert(chat1);
+            });
         }
     };
     private ConnectionListener mConnectionListener = new ConnectionListener() {
@@ -187,7 +233,8 @@ public class XMPPService extends Service {
 
         @Override
         public void connectionClosedOnError(Exception e) {
-            Timber.i("server disconnected by error : %s", e);
+            Timber.e("server disconnected by error");
+            Timber.i(e);
             //异常断开
             //如果系统版本大于21（5.0）执行连接任务
             //否则通过广播连接
