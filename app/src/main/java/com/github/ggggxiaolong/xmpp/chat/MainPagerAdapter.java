933d7f385b40fc01db5e67b82f2778efc82fce5e
mrtan <1120610800@qq.com>
2016-10-05 23:03:01 +0800

    修改了点什么

---
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/chat/MainPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.github.ggggxiaolong.xmpp.chat;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.github.ggggxiaolong.xmpp.R;
+import com.github.ggggxiaolong.xmpp.utils.ObjectHolder;
+
+/**
+ * @author mrtan on 10/4/16.
+ */
+
+final class MainPagerAdapter extends FragmentStatePagerAdapter {
+    private static int[] icons = new int[]{R.drawable.ic_tab_chat,R.drawable.ic_tab_contact,R.drawable.ic_tab_found,R.drawable.ic_tab_me};
+    private static int[] title = new int[]{R.string.prompt_tab_chat,R.string.prompt_tab_contact,R.string.prompt_tab_found,R.string.prompt_tab_me};
+
+    MainPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:{
+                return new SessionFragment();
+            }
+            case 1:{
+                return new ContactFragment();
+            }
+            default:{
+                return new TempFragment();
+            }
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return ObjectHolder.context.getString(title[position]);
+    }
+
+    static void setTabIcon(TabLayout tabLayout){
+        int tabCount = tabLayout.getTabCount();
+        if (tabCount == icons.length){
+            for (int i = 0; i < tabCount; i++) {
+                TabLayout.Tab tab = tabLayout.getTabAt(i);
+                tab.setIcon(icons[i]);
+            }
+        }
+    }
+}
