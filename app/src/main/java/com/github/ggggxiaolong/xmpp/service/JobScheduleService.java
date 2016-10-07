2e00473df8968df070315287c15ddc42fdce465d
mrtan <1120610800@qq.com>
2016-10-07 10:45:34 +0800

    修复bug

--- app/src/main/java/com/github/ggggxiaolong/xmpp/service/JobScheduleService.java
+++ app/src/main/java/com/github/ggggxiaolong/xmpp/service/JobScheduleService.java
@@ -5,6 +5,7 @@ import android.app.job.JobService;
 import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
 import android.support.annotation.RequiresApi;
@@ -22,12 +23,41 @@ import timber.log.Timber;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public final class JobScheduleService extends JobService {
+    private Handler mHandler ;
+    private Looper mLooper;
+
+    {
+        HandlerThread thread = new HandlerThread("jobThread", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+        mLooper = thread.getLooper();
+        mHandler = new Handler(mLooper) {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case 1: {
+                        try {
+                            XMPPUtil.connect(null);
+                            XMPPUtil.login();
+                        } catch (Exception e) {
+                            Timber.e(e);
+                        }
+                        //当任务执行完毕之后，你需要调用jobFinished()来让系统知道这个任务已经结束，
+                        // 系统可以将下一个任务添加到队列中。如果你没有调用jobFinished()，
+                        // 你的任务只会执行一次，而应用中的其他任务就不会被执行
+                        jobFinished((JobParameters) msg.obj, true);
+                        break;
+                    }
+                }
+            }
+        };
+    }
     @Override
     public boolean onStartJob(JobParameters params) {
         //如果返回值是false,系统假设这个方法返回时任务已经执行完毕。
         // 如果返回值是true,那么系统假定这个任务正要被执行，执行任务的重担就落在了你的肩上。
         // 当任务执行完毕时你需要调用jobFinished()来通知系统
         if (XMPPUtil.isConnected()) {
+            mLooper.quit();
             return false;
         } else {
             mHandler.sendMessage(mHandler.obtainMessage(1, params));
@@ -40,26 +70,4 @@ public final class JobScheduleService extends JobService {
         return false;
     }
 
-    private HandlerThread mThread = new HandlerThread("jobThread", Process.THREAD_PRIORITY_BACKGROUND);
-
-    private Handler mHandler = new Handler(mThread.getLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case 1: {
-                    try {
-                        XMPPUtil.connect(null);
-                        XMPPUtil.login();
-                    } catch (Exception e) {
-                        Timber.e(e);
-                    }
-                    //当任务执行完毕之后，你需要调用jobFinished()来让系统知道这个任务已经结束，
-                    // 系统可以将下一个任务添加到队列中。如果你没有调用jobFinished()，
-                    // 你的任务只会执行一次，而应用中的其他任务就不会被执行
-                    jobFinished((JobParameters) msg.obj, true);
-                    break;
-                }
-            }
-        }
-    };
 }
